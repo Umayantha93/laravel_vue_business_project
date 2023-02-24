3d67361f60622648e847be66f71091167ded4475
Umayantha93 <umayantha93@gmail.com>
2023-02-24 11:16:17 +0530

    setup and install dependencies

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,16 @@
+<template>
+
+        <div>Index</div>
+        <Link href="/hello">Show Page</Link>
+        <div>message is {{ message }}</div>
+
+</template>
+
+
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+    defineProps({
+        message: String
+    });
+</script>
+
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,10 @@
+<template>
+        <div>Show</div>
+        <Link href="/hello">Index Page</Link>
+</template>
+
+
+<script setup>
+    import { Link } from '@inertiajs/vue3'
+</script>
+
